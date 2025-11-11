a4d29550b46e71e3bd9d3b74b8437cb69f0a55cd
silascyrax001 <jhustbright@gmail.com>
2025-11-12 00:13:16 +0100

    feat: create Checkbox component

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,40 @@
+import { forwardRef, InputHTMLAttributes } from 'react';
+import { cn } from '@/lib/utils/cn';
+
+export interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
+  label?: string;
+}
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, label, id, ...props }, ref) => {
+    const checkboxId = id || `checkbox-${Math.random().toString(36).substr(2, 9)}`;
+    
+    return (
+      <div className="flex items-center">
+        <input
+          id={checkboxId}
+          type="checkbox"
+          ref={ref}
+          className={cn(
+            'h-4 w-4 rounded border-gray-300 text-blue-600',
+            'focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
+            'disabled:cursor-not-allowed disabled:opacity-50',
+            className
+          )}
+          {...props}
+        />
+        {label && (
+          <label
+            htmlFor={checkboxId}
+            className="ml-2 text-sm font-medium text-gray-700 cursor-pointer"
+          >
+            {label}
+          </label>
+        )}
+      </div>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
+
