64d9762bbb6ad50889b9219dc0d04cfa463f9f0a
silascyrax001 <jhustbright@gmail.com>
2025-11-11 23:34:09 +0100

    feat: create throttle utility

---
+++ src/lib/utils/throttle.ts
@@ -0,0 +1,25 @@
+// Throttle utility for performance optimization
+
+/**
+ * Throttle function calls
+ * @param func - Function to throttle
+ * @param limit - Time limit in milliseconds
+ * @returns Throttled function
+ */
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean;
+  
+  return function executedFunction(...args: Parameters<T>) {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => {
+        inThrottle = false;
+      }, limit);
+    }
+  };
+}
+
