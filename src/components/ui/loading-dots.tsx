2d724382366b98524a2d78d11be5e9cd228b2415
silascyrax001 <jhustbright@gmail.com>
2025-11-12 11:21:00 +0100

    feat: create LoadingDots animation component

---
+++ src/components/ui/loading-dots.tsx
@@ -0,0 +1,10 @@
+export function LoadingDots() {
+  return (
+    <div className="flex space-x-1">
+      <div className="w-2 h-2 bg-blue-600 rounded-full animate-bounce" style={{ animationDelay: '0ms' }}></div>
+      <div className="w-2 h-2 bg-blue-600 rounded-full animate-bounce" style={{ animationDelay: '150ms' }}></div>
+      <div className="w-2 h-2 bg-blue-600 rounded-full animate-bounce" style={{ animationDelay: '300ms' }}></div>
+    </div>
+  );
+}
+
