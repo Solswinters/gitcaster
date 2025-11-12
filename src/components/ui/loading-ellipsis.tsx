b780c96ce1c9104b6d2534d1a786b0bbffd5441e
silascyrax001 <jhustbright@gmail.com>
2025-11-12 13:00:01 +0100

    feat: create LoadingEllipsis animation

---
+++ src/components/ui/loading-ellipsis.tsx
@@ -0,0 +1,10 @@
+export function LoadingEllipsis() {
+  return (
+    <span className="inline-flex space-x-1">
+      <span className="w-1.5 h-1.5 bg-current rounded-full animate-pulse" style={{ animationDelay: '0ms' }}></span>
+      <span className="w-1.5 h-1.5 bg-current rounded-full animate-pulse" style={{ animationDelay: '200ms' }}></span>
+      <span className="w-1.5 h-1.5 bg-current rounded-full animate-pulse" style={{ animationDelay: '400ms' }}></span>
+    </span>
+  );
+}
+
