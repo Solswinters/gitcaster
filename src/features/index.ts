7f6db7b4693a552ec04efc3613bbc2e0b4c8122f
silascyrax001 <jhustbright@gmail.com>
2025-11-13 12:52:06 +0100

    Add features barrel export for centralized imports

---
+++ src/features/index.ts
@@ -0,0 +1,18 @@
+/**
+ * Features
+ * 
+ * Centralized export for all feature modules
+ */
+
+// Auth feature
+export * from './auth';
+
+// GitHub feature
+export * from './github';
+
+// Profile feature
+export * from './profile';
+
+// Search feature
+export * from './search';
+
