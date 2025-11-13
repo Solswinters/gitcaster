bba612d8c2f73e2a7187c462c20ba59ca595f332
silascyrax001 <jhustbright@gmail.com>
2025-11-13 12:54:54 +0100

    Add shared module barrel export for centralized access

---
+++ src/shared/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Shared Module
+ * 
+ * Centralized export for all shared functionality
+ */
+
+// Types
+export * from './types';
+
+// Utilities
+export * from './utils';
+
+// Components
+export * from './components/loading';
+
