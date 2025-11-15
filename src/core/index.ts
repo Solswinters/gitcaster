2b6cb71c2b5aa6a5dd24b83e352e5559a16016a4
silascyrax001 <jhustbright@gmail.com>
2025-11-15 22:14:44 +0100

    Update core module exports structure

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,6 @@
-export * from './components';
-export * from './hooks';
-export * from './utils';
-export * from './types';
-export * from './lib';
-export * from './config';
-
+export * as Components from './components';
+export * as Hooks from './hooks';
+export * as Utils from './utils';
+export * as Types from './types';
+export * as Lib from './lib';
+export * as Config from './config';
