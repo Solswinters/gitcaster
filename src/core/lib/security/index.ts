56e097406347ca87df7f41eb9d6aafcd219399ee
silascyrax001 <jhustbright@gmail.com>
2025-11-15 22:14:10 +0100

    Move security utilities to core infrastructure

--- src/core/lib/security/index.ts
+++ src/core/lib/security/index.ts
@@ -1,1 +1,5 @@
-export {};
+export * from './content-security-policy';
+export * from './csrf-protection';
+export * from './input-sanitization';
+export * from './rate-limiter';
+export * from './security-headers';
