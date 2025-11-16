0128bcbc204c75db108fb39ebd8d875ba33b21cb
silascyrax001 <jhustbright@gmail.com>
2025-11-16 23:18:07 +0100

    Update central utils exports with all utility modules

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -14,14 +14,20 @@ export * as UrlUtils from './url';
 export * as FileUtils from './file';
 export * as CryptoUtils from './crypto';
 export * as ColorUtils from './color';
+export * as DeviceUtils from './device';
+export * as PerformanceUtils from './performance';
 
 // Re-export commonly used utilities
-export { capitalizeFirstLetter, truncateString } from './string';
-export { formatNumber, formatCurrency, formatPercentage } from './number';
-export { formatDate, formatDateRelative } from './date';
-export { unique, chunk, groupBy } from './array';
-export { pick, omit, deepClone } from './object';
+export { capitalizeFirstLetter, truncateString, toKebabCase } from './string';
+export { formatNumber, formatCurrency, formatPercentage, formatBytes } from './number';
+export { formatDate, formatDateRelative, addDays } from './date';
+export { unique, chunk, groupBy, shuffle } from './array';
+export { pick, omit, deepClone, isEmpty } from './object';
 export { debounce, throttle, delay, retry } from './async';
 export { isValidEmail, isStrongPassword, isValidUrl } from './validation';
-export { copyToClipboard, scrollToTop } from './browser';
+export { copyToClipboard, scrollToTop, setLocalStorage, getLocalStorage } from './browser';
+export { hexToRgb, rgbToHex } from './color';
+export { hashString, generateUUID } from './crypto';
+export { isMobile, isDesktop, getDeviceType } from './device';
+export { measurePerformance, PerformanceTimer } from './performance';
 export { cn } from './cn';
