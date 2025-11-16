31a79cacca9e2f92aef82e1ae1042412ad0e6db5
silascyrax001 <jhustbright@gmail.com>
2025-11-16 23:16:15 +0100

    Update central utilities exports

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,3 +1,27 @@
-export * from './cn';
-export * from './format';
-export * from './validation';
+/**
+ * Central exports for all core utilities
+ */
+
+export * as StringUtils from './string';
+export * as NumberUtils from './number';
+export * as DateUtils from './date';
+export * as ArrayUtils from './array';
+export * as ObjectUtils from './object';
+export * as AsyncUtils from './async';
+export * as ValidationUtils from './validation';
+export * as BrowserUtils from './browser';
+export * as UrlUtils from './url';
+export * as FileUtils from './file';
+export * as CryptoUtils from './crypto';
+export * as ColorUtils from './color';
+
+// Re-export commonly used utilities
+export { capitalizeFirstLetter, truncateString } from './string';
+export { formatNumber, formatCurrency, formatPercentage } from './number';
+export { formatDate, formatDateRelative } from './date';
+export { unique, chunk, groupBy } from './array';
+export { pick, omit, deepClone } from './object';
+export { debounce, throttle, delay, retry } from './async';
+export { isValidEmail, isStrongPassword, isValidUrl } from './validation';
+export { copyToClipboard, scrollToTop } from './browser';
+export { cn } from './cn';
