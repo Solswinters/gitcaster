263b0359d5b222acc1f6cdf23cf1099b505aa884
silascyrax001 <jhustbright@gmail.com>
2025-11-16 23:24:54 +0100

    Add loading utilities and helpers

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -4,6 +4,7 @@
 
 export * as ErrorComponents from './error';
 export * from './error';
+export * from './loading';
 
 // Re-export commonly used error components
 export { ErrorBoundary, ErrorMessage } from './error';
