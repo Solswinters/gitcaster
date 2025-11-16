6f0865cbf27101fea101cf2cab6dbddfb2306d68
silascyrax001 <jhustbright@gmail.com>
2025-11-16 23:20:41 +0100

    Update core exports to include error handling

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -1,2 +1,9 @@
-export {};
+/**
+ * Central exports for core components
+ */
 
+export * as ErrorComponents from './error';
+export * from './error';
+
+// Re-export commonly used error components
+export { ErrorBoundary, ErrorMessage } from './error';
--- src/core/index.ts
+++ src/core/index.ts
@@ -2,7 +2,21 @@
  * Central exports for core infrastructure
  */
 
-export * from './utils';
-export * from './config';
-export * from './constants';
+export * as Errors from './errors';
+export * as Utils from './utils';
+export * as Components from './components';
+export * as Config from './config';
+export * as Constants from './constants';
 export * from './lib';
+
+// Re-export commonly used items
+export { cn } from './utils/cn';
+export { HTTP_STATUS } from './constants/http';
+export { 
+  AppError,
+  ValidationError,
+  NotFoundError,
+  ErrorHandler,
+  ErrorTracker,
+} from './errors';
+export { ErrorBoundary, ErrorMessage } from './components/error';
