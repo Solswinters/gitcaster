6f7b856226e2a69aedd1e3439b9ac66a48c0ab88
silascyrax001 <jhustbright@gmail.com>
2025-11-25 23:05:56 +0100

    refactor(code): standardize imports in TeamCard.test.tsx

--- tests/unit/features/collaboration/TeamCard.test.tsx
+++ tests/unit/features/collaboration/TeamCard.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { render, screen, fireEvent } from '@testing-library/react';
+
 import { TeamCard, TeamMember } from '@/features/collaboration/components/TeamCard';
 
 const mockMembers: TeamMember[] = [
