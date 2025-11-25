caa1ad36cce278e87eb4dbd3e58a156cf5512a61
silascyrax001 <jhustbright@gmail.com>
2025-11-25 23:05:35 +0100

    refactor(code): standardize imports in client.ts

--- src/lib/github/client.ts
+++ src/lib/github/client.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 import { GitHubUser, GitHubRepo, GitHubCommit, GitHubPullRequest, GitHubIssue, LanguageStats, ContributionDay } from '@/types';
 
 const GITHUB_API_BASE = 'https://api.github.com';
