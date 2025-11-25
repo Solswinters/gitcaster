7112677c0e6a463cf6d4ff37082df36a46191158
silascyrax001 <jhustbright@gmail.com>
2025-11-25 23:06:12 +0100

    refactor(code): standardize imports in Banner.test.tsx

--- tests/unit/shared/components/Banner.test.tsx
+++ tests/unit/shared/components/Banner.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { render, fireEvent } from '@testing-library/react';
+
 import { Banner } from '@/shared/components/feedback/Banner';
 
 describe('Banner', () => {
